8217c2d0f2e82da5d2d52a2edb3d110f363ac887
Nguyen Hai Dang <nguyenhaidang04072004@gmail.com>
2024-08-18 17:11:19 +0700

    add train

--- data_loader.py
+++ data_loader.py
@@ -3,14 +3,29 @@ import numpy as np
 import cv2
 
 
+def process_data(img_path, target_size=(128, 128)):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, target_size)
+    img = img.astype('float32') / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def img_loader(path_dir):
+    path_imgs = []
+    for img in os.listdir(path_dir):
+        path_img = path_dir + '/' + img
+        path_imgs.append(path_img)
+    return path_imgs
+
 
 def data_loader(path_dir):
-    path_imgs = []
-    for i in os.listdir(path_dir):
-        print(i)
-        for j in os.listdir(os.path.join(path_dir, i)):
-            path_imgs.append(os.path.join(path_dir, i, j))
+    imgs = []
+    for img in img_loader(path_dir):
+        img = process_data(img)
+        imgs.append(img)
+    return np.vstack(imgs)
 
-
-
-data_loader('data')
+print(img_loader('data'))
+# print(data_loader('data'))
--- train.py
+++ train.py
@@ -1,3 +1,23 @@
 import tensorflow as tf
 
 
+x = tf.keras.Sequential()
+x.add(tf.keras.layers.Conv2D(32, (3, 3), (1, 1), use_bias=False))
+x.add(tf.keras.layers.BatchNormalization())
+x.add(tf.keras.layers.Activation('relu'))
+
+x.add(tf.keras.layers.Conv2D(128, (3, 3), (1, 1), use_bias=False))
+x.add(tf.keras.layers.BatchNormalization())
+x.add(tf.keras.layers.Activation('relu'))
+x.add(tf.keras.layers.MaxPool2D((2, 2), strides=(1, 1)))
+
+x.add(tf.keras.layers.Dense(1024, activation='relu'))
+x.add(tf.keras.layers.Dropout(0.2))
+x.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+x.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+    metrics=['accuracy']
+)
+history = x.fit(x=None, batch_size=32, epochs=20, validation_data=None)
+print(history)
