0e5f287c71af5af6ef07afffb53d40a34018ac5e
Nguyen Hai Dang <nguyenhaidang04072004@gmail.com>
2025-01-07 12:24:04 +0700

    dataloader was done

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,12 @@
 import os
 import cv2
+from PIL import Image
 from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import ToTensor, Compose, Resize
 
 class CatsAndDogsDataset(Dataset):
-    def __init__(self, path, is_train):
+    def __init__(self, path, is_train, transform):
+        self.transform = transform
         self.classes = ['cats', 'dogs']
         if is_train:
             data_path = os.path.join(path, 'training_set')
@@ -13,26 +16,41 @@ class CatsAndDogsDataset(Dataset):
         self.images = []
         self.labels = []
 
-        for class_name in self.classes:
+        for class_id, class_name in enumerate(self.classes):
             sub_folder_path = os.path.join(data_path, class_name)
             for image_name in os.listdir(sub_folder_path):
                 image_path = os.path.join(sub_folder_path, image_name)
-                self.images.extend(image_path)
-                self.labels.extend(self.labels)
+                self.images.append(image_path)
+                self.labels.append(class_id)
+                # print(self.images)
 
     def __len__(self):
-        pass
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        image = cv2.imread(self.images[idx], cv2.COLOR_BGR2RGB)
+        # image = cv2.imread(self.images[idx])
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.open(self.images[idx]).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
         label = self.labels[idx]
 
         return image, label
 
 
 if __name__ == "__main__":
-    dataset = CatsAndDogsDataset(path='datasource', is_train=True)
-    image, label = dataset[100]
+    transform = Compose([
+        ToTensor(),
+        Resize((224, 224))
+    ])
+    dataset = CatsAndDogsDataset(path='datasource', is_train=True, transform=transform)
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=16,
+        shuffle=True,
+        num_workers=4,
+        drop_last=True
+    )
 
-    print(image.shape)
-    print(label)
+    for image, label in dataloader:
+        print(image.shape, label)
--- main.py
+++ main.py
@@ -1,44 +1,5 @@
-import requests
-from flask import Flask, render_template, request, jsonify
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
+from PIL import Image
 
+image = Image.open("datasource/training_set/cats/cat.78.jpg")
 
-def preprocess_image(img_path, target_size):
-    img = image.load_img(img_path, target_size=target_size)
-    img_array = image.img_to_array(img)
-    img_scale = np.expand_dims(img_array, axis=0) / 255.0
-    return img_scale
-
-app = Flask(__name__)
-
-model = load_model('model/cats_dogs_cls.h5')
-# image = preprocess_image('datasets/val/dogs/dog.5.jpg', target_size=(128, 128))
-# prediction = model.predict(image)
-# print(prediction[0][0])
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if 'file' not in request.files:
-        return "No file part"
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file"
-    if file:
-        # Preprocess the image
-        image = preprocess_image(file, target_size=(128, 128))
-
-        # Make prediction
-        prediction = model.predict(image)
-        predicted_class = "Dog" if prediction[0][0] > 0.5 else "Cat"
-
-        return jsonify({'prediction': predicted_class})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+image.show()
